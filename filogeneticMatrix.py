a3a27a68da662d47bef526d438116090b85b40fe
Francesco Ribaudo <fra.ribaudo95@gmail.com>
2018-04-14 18:01:34 +0200

    Shuffle function and Argument control added

--- filogeneticMatrix.py
+++ filogeneticMatrix.py
@@ -5,30 +5,58 @@ import sys
 import re
 import random
 
-print(len(sys.argv))
+def shuffle(match):
+    nodeList = map(lambda x: x[0], match)
+    nodeList = map(lambda x: x[1], match)
+    charList = map(lambda x: x[2], match)
+    edgeList = list()
+
+    nodeList = list(set(nodeList))
+    for i in range(len(match)):
+        edgeList.append((random.choice(nodeList), random.choice(nodeList), random.choice(charList)))
+    return edgeList
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 with open(sys.argv[1],'r') as input_file:
     file = input_file.read()
+#Match = Lista di tuple (node1, node2, carattere)
+match = re.findall('^(\d+)\s+(\d+)\s+\{\'weight\'\:\s+([-]*\d+)\}$',file,re.M)
 
+if len(sys.argv) == 2:
+    #STAMPA MATRICE FILOGENETICA
+    print("Caso 1")
+elif len(sys.argv) == 3:
+    if sys.argv[2].lower() == "--shuffle":
+        match = shuffle(match)
+elif len(sys.argv) == 4:
+    if sys.argv[2].lower() == "--rows" and is_number(sys.argv[3]) and int(sys.argv[3]) <= len(match):
+        n = int(sys.argv[3])
+        print(n)
+        #TO DO
+    else:
+        print("Error: Invalid argument!")
+        sys.exit()            
+elif len(sys.argv) == 5:
+    if sys.argv[2].lower() == "--shuffle" and sys.argv[3].lower() == "--rows" and is_number(sys.argv[4]) and int(sys.argv[4]) <= len(match):
+        match = shuffle(match)
+        n = int(sys.argv[4])
+        print(n)
+        #TO DO
+    if sys.argv[2].lower() == "--rows" and is_number(sys.argv[3]) and int(sys.argv[3]) <= len(match) and sys.argv[4].lower() == "--shuffle":
+        n = int(sys.argv[3])
+        match = shuffle(match)
+        print(n)
+        #TO DO
+    else:
+        print("Error: Invalid argument!")
+        sys.exit()    
+else:
+    print("Error: Invalid argument!")
+    sys.exit()
 
-match=re.findall('^(\d+)\s+(\d+)\s+\{\'weight\'\:\s+([-]*\d+)\}$',file,re.M)
-flag=True
-print(match)
-print(len(match))
-
-#Funzione RANDOM vuole mischiare nodi con nodi e caratteri con caratteri 
-#funzione = input('Che funzione vuoi utilizzare: (Random o RowsN)')
-#funzione=funzione.lower()
-#if funzione=='random':
-#    print("Funzione Random: Verranno mischiati i nodi e verrà creato un nuovo albero")
-#    random.shuffle(match)#Sbagliato 
-#    print(match)
-#Funzione ROWSN:considero solo N nodi (I nodi sono massimo 131)
-#elif funzione=='rowns':
-#    print("Funzione RowsN: Verranno considerati i primi N nodi")
-#    nodes= int(input("Inserisci il numero di nodi che vuoi considerare: "))
-#    while flag:
-#        if nodes>=len(match):
-#            print("Il numero supera il numero massimo di nodi.")
-#            nodes=input("Inserisci un nuove numero:")
-#        else:
-#            flag=False
