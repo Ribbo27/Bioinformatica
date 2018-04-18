6f728d4dbf85c299c47a5e02e81cac387625c75b
Francesco Ribaudo <fra.ribaudo95@gmail.com>
2018-04-18 16:08:19 +0200

    Update

--- filogeneticMatrix.py
+++ filogeneticMatrix.py
@@ -16,19 +16,6 @@ def builder(nodeList, charList):
     Matrix = [[2 for x in range(characters)] for y in range(nodes)]
     return Matrix
 
-def findRoot(edgeList):
-    roots = map(lambda x: x[0], edgeList) # List of possible roots
-    nodes = map(lambda x: x[1], edgeList) # List of internal nodes
-    c = 0                                 # Counter variable
-    for i in range(len(roots)):
-        tmp = roots[i]                    # Possible root
-        for j in nodes:
-            c = nodes.count(tmp)        
-            if c == 0:                    # Check for root
-                return tmp 
-    print("Error: root not found!")       # You should never get here
-    sys.exit()
-
 def matrixGen(edgeList, nodeList, matrix):
 
     route = []
@@ -44,31 +31,6 @@ def matrixGen(edgeList, nodeList, matrix):
             print node
     
     #print route
-        
-
-
-def shuffle(edgeList):
-    ''' 
-        Shuffle is a function that mixes every 
-        tuple's element inside a list
-
-        The tuple are such (node1, node2, char)
-        where node1 and node2 represent two nodes
-        of a filogenetic tree while char represents
-        the genetic character
-
-        Keyword argument:
-        --edgelist: List of tuples
-
-        Return the edgetreelist shuffled
-    '''
-    
-    randEdgeList = list()
-
-    nodeList = list(set(nodeList))
-    for i in range(len(edgeList)):
-        randEdgeList.append((random.choice(nodeList), random.choice(nodeList), random.choice(charList)))
-    return randEdgeList
 
 def is_number(s):
     '''
@@ -106,24 +68,19 @@ charList = map(lambda x: int(x[2]), edgeList)           # List of all characters
 
 charList.sort()
 
-print charList
-
-#matrix = builder(nodeList, charList)
+matrix = builder(nodeList, charList)
 
 #listaTest = [(1, 5, 9), (4, 6, 0), (1, 7, 3), (3, 8, 0), (4, 3, 7), (3, 2, 6), (1, 4, 12)]
 #nodeTest = [1, 5, 4, 6, 7, 3, 8, 2]
 
-#root = findRoot(edgeList)
-
-#matrixGen(edgeList, nodeList, matrix)
-#print root
+matrixGen(edgeList, nodeList, matrix)
 
 if len(sys.argv) == 2:
-    #TO DO ********************
+    matrix = matrixGen(edgeList, nodeList, matrix)
     print("Caso 1")
 elif len(sys.argv) == 3:
     if sys.argv[2].lower() == "--shuffle":
-        randEdgeList = shuffle(edgeList)
+        print('prova')
 elif len(sys.argv) == 4:
     if sys.argv[2].lower() == "--rows" and is_number(sys.argv[3]) and int(sys.argv[3]) <= len(edgeList):
         n = int(sys.argv[3])
@@ -134,13 +91,11 @@ elif len(sys.argv) == 4:
         sys.exit()            
 elif len(sys.argv) == 5:
     if sys.argv[2].lower() == "--shuffle" and sys.argv[3].lower() == "--rows" and is_number(sys.argv[4]) and int(sys.argv[4]) <= len(edgeList):
-        randEdgeList = shuffle(edgeList)
         n = int(sys.argv[4])
         print(n)
         #TO DO ********************
     if sys.argv[2].lower() == "--rows" and is_number(sys.argv[3]) and int(sys.argv[3]) <= len(edgeList) and sys.argv[4].lower() == "--shuffle":
         n = int(sys.argv[3])
-        randEdgeList = shuffle(edgeList)
         print(n)
         #TO DO ********************
     else:
